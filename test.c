a27de1e6b754ff959c376a6f3188cd1d11a8044e
Stanislas MEDRANO <stanislas.medrano@epita.fr>
2021-03-19 22:46:10 +0100

    version-6.0

--- test.c
+++ test.c
@@ -7,6 +7,7 @@ int main(void)
     puts("New line");
     puts("Is my project running?");
     puts("Trying to push without tag");
-    puts("This time is will work with version-1.0");
+    puts("This time is will work with version-6.0");
+    puts("22:45");
     return 0;
 }
